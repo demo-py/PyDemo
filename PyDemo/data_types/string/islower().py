bb06a7621859cf611c9edc1ae19341fc27f543d6
demo <dazanderofficials@gmail.com>
2024-01-24 22:51:05 +0800

    str.islower()

---
+++ PyDemo/data_types/string/islower().py
@@ -0,0 +1,23 @@
+"""
+
+  str.islower()
+
+  Checks if all characters in the string are lowercase
+
+"""
+
+string : str = "pydemo"
+print(
+  string.islower()
+)
+
+# Output :
+# True
+
+# =====================
+
+string : str = "PyDemo"
+print(string.islower())
+
+# Output :
+# False
