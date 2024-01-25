3e7fb4b1ba954a2aca094469d8abd9578b7a928a
demo <dazanderofficials@gmail.com>
2024-01-25 22:41:06 +0800

    Checks whether all characters in the string are uppercase or not

---
+++ PyDemo/data_types/string/isupper().py
@@ -0,0 +1,21 @@
+"""
+
+  str.isupper()
+
+  Checks whether all characters in the string are uppercase or not
+
+"""
+
+string : str = "CAPSLOCKED"
+print(string.isupper())
+
+# Output :
+# True
+
+# ======================
+
+string : str = "lowered"
+print(string.isupper())
+
+# Output :
+# False
